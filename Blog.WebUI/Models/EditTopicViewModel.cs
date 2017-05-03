e475461f36c190e858304a89124ea04e941524ca
Lukasz Wardak <Lukasz Wardak>
2017-05-04 01:37:06 +0200

    Blog MVC

---
+++ Blog.WebUI/Models/EditTopicViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Blog.WebUI.Models
+{
+    public class EditTopicViewModel
+    {
+        public int TopicID { get; set; }
+        public string Title { get; set; }
+        public string Text { get; set; }
+        public DateTime AddedDate { get; set; }
+    }
+}
---
+++ Blog.WebUI/Models/NavigationItem.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Blog.WebUI.Models
+{
+    public class NavigationItem
+    {
+        public string LinkName { get; set; }
+        public string LinkAddress { get; set; }
+    }
+}
